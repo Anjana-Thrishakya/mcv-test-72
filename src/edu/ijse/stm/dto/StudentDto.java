e0e132b4ea21a0d989e2ef9082d51188ef970b57
Anjana Thrishakya <anjanathrishakya@gmail.com>
2024-09-05 14:08:40 +0530

    Student DTO

---
+++ src/edu/ijse/stm/dto/StudentDto.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.ijse.stm.dto;
+
+/**
+ *
+ * @author anjan
+ */
+public class StudentDto {
+    private String studentId;
+    private String name;
+    private String email;
+    private String course;
+
+    public StudentDto() {
+    }
+
+    public StudentDto(String studentId, String name, String email, String course) {
+        this.studentId = studentId;
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    /**
+     * @return the studentId
+     */
+    public String getStudentId() {
+        return studentId;
+    }
+
+    /**
+     * @param studentId the studentId to set
+     */
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the course
+     */
+    public String getCourse() {
+        return course;
+    }
+
+    /**
+     * @param course the course to set
+     */
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentDto{" + "studentId=" + studentId + ", name=" + name + ", email=" + email + ", course=" + course + '}';
+    }
+}
